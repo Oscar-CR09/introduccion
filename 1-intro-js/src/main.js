0c27d958a62597f6e0b1c9a9c5ff227fb74b43de
Oscar <oscarcr189@outlook.com>
2023-08-27 12:53:55 -0600

    objetos y metodos

--- 1-intro-js/src/main.js
+++ 1-intro-js/src/main.js
@@ -3,9 +3,22 @@ const invoice ={
     id: 10,
     name: 'Compras de oficina',
     date: new Date(),
-    client: 'Oscar',
+    client: {
+        id: 2,
+        name:'jhon x',
+        lastname:'x',
+        age:20,
+    },
     total: 1000,
 
+    greeting:function(){
+        return `Hola ${this.client.name}`;
+    }
+
 };
-invoice.client = 'skar';
+
+//invoice.client.name = 'skar';
+invoice.total= 2000;
 console.log(invoice);
+const greeting = invoice.greeting();
+console.log(greeting);
