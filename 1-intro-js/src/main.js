011ed3ab3d74f524a03d4188b4987c2245bcc777
Oscar <oscarcr189@outlook.com>
2023-08-27 13:03:15 -0600

    arreglos de objetos

--- 1-intro-js/src/main.js
+++ 1-intro-js/src/main.js
@@ -5,11 +5,43 @@ const invoice ={
     date: new Date(),
     client: {
         id: 2,
-        name:'jhon x',
+        name:'Oscar',
         lastname:'x',
         age:20,
     },
-    total: 1000,
+
+    items:[
+        {
+            producto:'teclado',
+            price: 399,
+            cantidad: 2
+        }
+        ,
+        {
+            producto:"mouse",
+            price: 100,
+            cantidad: 10
+
+        }
+        ,
+        {
+            producto:"papel",
+            price: 389,
+            cantidad: 3
+
+        }
+    ],
+
+
+
+    total: function (){
+        let total = 0;
+        this.items.forEach(item =>{
+            total = total + item.price*item.cantidad;
+
+        });
+        return total;
+    },
 
     greeting:function(){
         return `Hola ${this.client.name}`;
@@ -18,7 +50,11 @@ const invoice ={
 };
 
 //invoice.client.name = 'skar';
-invoice.total= 2000;
+//invoice.total= 2000;
+
 console.log(invoice);
+
 const greeting = invoice.greeting();
 console.log(greeting);
+console.log(`total: ${invoice.total()}`);
+
