95a55dfb19e92843bec919fd0d6c239d36157610
Oscar <oscarcr189@outlook.com>
2023-08-26 23:06:12 -0600

    funciones de flecha

--- 1-intro-js/src/main.js
+++ 1-intro-js/src/main.js
@@ -1,6 +1,6 @@
 
 
-function sayHello(name, age) {
+/*const sayHello = function sayHello(name, age) {
 
     const greeting= `Hola Mundo function! ${name, age}`;
 
@@ -8,7 +8,13 @@ function sayHello(name, age) {
 
     return greeting;
 }
+*/
+//exprecion lamda ****************
+
+const sayHello = (name, age)=> `Hola Mundo function! ${name, age}`;
+const add = (a, b)=> a+b;     
 
 const result = sayHello('Oscar', 10);
 
 console.log(result);
+console.log(add (10,5));
