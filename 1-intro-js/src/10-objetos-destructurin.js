b4e6e400468e8e4ee0ad6b596406e9635b7acdde
Oscar <oscarcr189@outlook.com>
2023-08-27 19:14:04 -0600

    objetos destructurin

---
+++ 1-intro-js/src/10-objetos-destructurin.js
@@ -0,0 +1,17 @@
+
+
+const user = {
+    username: 'oscar',
+    email: 'oscar@gmail.com',
+    age:20,
+    ranking:9,
+}
+
+const details = (username,email) => { 
+    //const {username,email}=user;
+
+    console.log(`El detalle del usuario ${username} con correo ${email}`)
+}
+details(user);
+
+
--- 1-intro-js/src/main.js
+++ 1-intro-js/src/main.js
@@ -1,18 +1,2 @@
 
 
-const user = {
-    username: 'oscar',
-    email: 'oscar@gmail.com',
-    age:20,
-    ranking:9,
-}
-
-//const username = user.username;
-//const ranking = user.ranking;
-//const age = user.age;
-
-//const {username,ranking,age}=user;
-
-console.log(`${user.username} tiene ${user.age} de edad`);
-console.log(user.ranking);
-
